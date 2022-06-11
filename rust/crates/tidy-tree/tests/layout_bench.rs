0cde6ef4defbb788f05924453cc15352500bf7b1
Zixuan Chen <remch183@outlook.com>
2022-06-12 04:45:13 +0800

    test: add wasm bench in rust

--- rust/crates/tidy-tree/tests/layout_bench.rs
+++ rust/crates/tidy-tree/tests/layout_bench.rs
@@ -2,10 +2,11 @@
 
 mod aesthetic_rules;
 mod gen;
+
 use rand::{prelude::StdRng, SeedableRng};
 use tidy_tree::{BasicLayout, Layout, TidyLayout};
 extern crate test;
-use test::Bencher;
+use test::{black_box, Bencher};
 
 #[bench]
 fn bench_tidy_layout(bench: &mut Bencher) {
@@ -16,9 +17,23 @@ fn bench_tidy_layout(bench: &mut Bencher) {
         peer_margin: 10.,
     };
 
-    bench.iter(|| {
+    bench.iter(black_box(|| {
         layout.layout(&mut tree);
-    });
+    }));
+}
+
+#[bench]
+fn bench_tidy_layout_large(bench: &mut Bencher) {
+    let mut rng = StdRng::seed_from_u64(1001);
+    let mut tree = gen::gen_tree(&mut rng, 1_000_000);
+    let layout = TidyLayout {
+        parent_child_margin: 10.,
+        peer_margin: 10.,
+    };
+
+    bench.iter(black_box(|| {
+        layout.layout(&mut tree);
+    }));
 }
 
 #[bench]
@@ -30,7 +45,7 @@ fn bench_naive_layout(bench: &mut Bencher) {
         peer_margin: 10.,
     };
 
-    bench.iter(|| {
+    bench.iter(black_box(|| {
         layout.layout(&mut tree);
-    });
+    }));
 }
---
+++ rust/crates/wasm/tests/bench.rs
@@ -0,0 +1,44 @@
+use rand::{prelude::StdRng, SeedableRng};
+use wasm::Tidy;
+use web_sys::{self, Performance};
+
+mod gen;
+use gen::gen_tree;
+extern crate wasm_bindgen_test;
+use wasm_bindgen_test::*;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+fn bench_tidy() {
+    let performance = get_performance();
+    let mut rng = StdRng::seed_from_u64(1001);
+    let tree = gen_tree(&mut rng, 100_000);
+    let mut layout = Tidy::with_tidy_layout();
+    tree.pre_order_traversal(|node| {
+        if node.parent.is_some() {
+            layout.add_node(
+                node.id,
+                node.width,
+                node.height,
+                unsafe { node.parent.unwrap().as_ref() }.id,
+            );
+        } else {
+            layout.add_node(node.id, node.width, node.height, Tidy::null_id());
+        }
+    });
+    let start = performance.now();
+    let times = 8;
+    for _ in 0..times {
+        layout.layout();
+    }
+    console_log!("{}ms", (performance.now() - start) / (times as f64));
+}
+
+fn get_performance() -> Performance {
+    let window = web_sys::window().expect("should have a window in this context");
+    let performance = window
+        .performance()
+        .expect("performance should be available");
+    performance
+}
