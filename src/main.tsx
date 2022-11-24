cba3798fc92ec863e3b327c777ecdbfe95c71434
Drew Hess <src@drewhess.com>
2022-11-24 16:53:35 +0000

    chore: Upgrade to React v18.
    Note that `npm install` complains about some React dependencies as a
    result of this upgrade, but they're all Storybook dependencies that
    are stuck on pre-v18 versions, and in my testing, Storybook appears to
    work fine, in any case.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
+const rootElement: HTMLElement | null = document.getElementById('root');
+if (!rootElement) {
+  throw new Error(
+    "The HTML root element doesn't exist. Please report this error!",
+  );
+}
+
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
