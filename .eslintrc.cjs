975f83416e7bcbe6e0fbca03171eeb77403e2d54
Drew Hess <src@drewhess.com>
2022-11-26 23:30:16 +0000

    feat: eslint support.

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+
+  extends: [
+    'eslint:recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+    'prettier',
+  ],
+
+  settings: {
+    react: {
+      // Stop eslint-plugin-react from complaining.
+      version: '17',
+    },
+
+    // This keeps the eslint-plugin-import linter in sync with our
+    // TypeScript settings, so we only need to set up path aliases in
+    // tsconfig.json.
+    'import/resolver': {
+      typescript: {
+        project: ['tsconfig.json'],
+      },
+    },
+  },
+};
